3fd0ab5610df196bf4ddd17355a20d6db9a1f20e
Dmitrio <ar-dimasik@ya.ru>
2022-05-05 12:16:15 +0200

    Homework 1.3

--- src/js/__test__/app.test.js
+++
@@ -1,8 +0,0 @@
-import demo from '../app';
-
-test('function demo shoul return value', () => {
-  const input = 'result';
-  const expected = 'result';
-  // const received = new Bowerman(input[0], input[1]);
-  expect(demo(input)).toEqual(expected);
-});
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 // TODO: write code here
 
 // comment this to pass build
-// const unusedVariable = 'variable';
+const unusedVariable = 'variable';
+console.log(unusedVariable);
 
 // for demonstration purpose only
 export default function demo(value) {
